9b54a88880b9203150b5cc3175ca7aee30cdcd49
Vadim <NazarovvorazaN@mail.ru>
2023-01-23 20:03:04 +0600

    23.01.2023

---
+++ Telegram_bot_m1/2m/10_writing_modules_2/1_if_name_main.py
@@ -0,0 +1,6 @@
+import calc
+print(__name__)
+print(calc.__name__)
+
+if __name__ == '__main__':
+    pass
---
+++ Telegram_bot_m1/2m/10_writing_modules_2/7_sys_path.py
@@ -0,0 +1,11 @@
+import sys
+
+for i in sys.path:
+    print(i)
+
+print()
+
+sys.path.append('E:\Project on Python/telegram_bot')
+
+for i in sys.path:
+    print(i)
